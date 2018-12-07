6f6c046c6d53b03a0d386e30bebadddcf16d1d61
Daniel Goldman <dzgoldman@wesleyan.edu>
2018-12-08 00:59:22 +0800

    contract, first pass (no merkel proof)

--- contracts/GuessNumber.sol
+++ contracts/GuessNumber.sol
@@ -1,274 +1,109 @@
 pragma solidity ^0.4.23;
 pragma experimental ABIEncoderV2;
+contract FairPlay {
 
-contract GuessNumber {
-  enum ActionType {
-      GUESS, CLUE, REVEAL
-  }
-  struct Action{
-    uint8 guess;
-    bool clue;
-    string secret;
-    ActionType actionType;
-    address sender;
-  }
-  
-    address public disputer;
-    uint256 timeoutInterval;
+    address merchant;
+    address client;
+    bytes32 merkelRoot;
+    uint price;
+    uint dataPacketsCount;
+    // float?
+    address contractAddress;
     uint256  public timeout;
-  
-  struct GameState {
-      address whoseTurn;
-      address guesser;
-      address dealer;
-      bool[5] clues;
-      uint8[5] guesses;
-      bytes32 secretHash;
-      bool gameStarted;
-      uint stake;
-      uint seq;
-      address winner;
-      address contractAddress;
-  }
-  GameState public onChainState;
+    bool cashOutDispute;
+    bytes32 contentDispute;
+    bool settled;
 
-  event AcceptGame(GameState gameState);
-  event StartDispute(GameState gameState, address starter);
-  event EndDispute(GameState gameState);
-  event UpdateTimeout(uint256 newTimeout, address whoseTurn);
+    // timouts for dispute and for whole interaction for cashing out
+    
 
 
-
-  constructor(bytes32 _secretHash) public payable {
-    require(msg.value > 0);
-    uint8[5] tempGuesses;
-    bool[5]  tempClues;
-      
-    onChainState = GameState({
-        whoseTurn: msg.sender,
-        guesser: address(0),
-        dealer: msg.sender,
-        clues:tempClues,
-        guesses: tempGuesses,
-        secretHash: _secretHash,
-        gameStarted: false,
-        stake: msg.value,
-        seq: 0,
-        winner: address(0),
-        contractAddress: address(this)
-
-    });
-        timeoutInterval = 3600;
-        timeout =  2**256 - 1;
+  struct AppState {
+    uint clientBalance;
+    uint merchantBalance;
+    uint nonce;
+    // content, not merkelleef
+    bytes32 merkelLeaf;
+    bool mutualClose;
+    
   }
   
-    
-//   during a dispate: making a move directly ends it happily
-// jumping to a DIFFERNT later or = state leads to slashing
-// starting a dispute means jumping to state (and possibly aslo moving), or just moving
-
-  function applyActionToChainState(Action action) public payout updateTimeout {
-    //   ensure sender is the real sender:(require it instead of overwriting? whatever)
-    action.sender = msg.sender;
-    onChainState = applyAction(onChainState, action);
-    if (disputer == opponentOf (onChainState, msg.sender)){
-        endDispute();
-    } else if (disputer == address(0)){
-        startDispute();
-    }
-  }
-
-//   TODO is this safe, i.e, private should be impossible to call directly?
-  function startDispute() private  {
-       disputer = msg.sender;
-       emit StartDispute(getOnChainState(), msg.sender);
-  }
-
-    function endDispute() private  {
-       disputer = address(0);
-       emit EndDispute(onChainState);
-  }
-
-  
-  function getOnChainState() public view returns(GameState){
-      return onChainState;
-  }
-
-  function stateToFullDigest(GameState gameState) public pure returns (bytes32){
-        bytes32 inputAppHash = stateToHash(gameState);
-        // TODO: purify address this
-        return prefixed(keccak256(gameState.contractAddress, inputAppHash));
-  }
-
-//   Might not need this?
-   function stateToHash(GameState gameState) public pure returns (bytes32){
-        return keccak256(abi.encode(gameState));
-  }
-
-  function jumpToStateOnChain(GameState gameState, bytes sig, Action action) public payout updateTimeout returns (bool){
-    bytes32 inputStateHash = stateToFullDigest(gameState);
-    require(recoverSigner(inputStateHash, sig) == opponentOf(gameState, msg.sender));
-    
-    // happy case: players agreed on winner, stop here
-    if (gameState.winner != address(0) ){
-        return true;
-    }
-    
-    require(gameState.seq >= onChainState.seq);
-    // for dispute, but may as well always check: don't jump to redundant state (player could have honestly applied action)
-    require(stateToFullDigest(onChainState) != stateToFullDigest(gameState));
-
-
-    
-    if (disputer == address(0)){
-        onChainState = gameState;
-        // if initiating a dispute at current turn, have to make a move in same transaction (if not, or if this is settling a dispute, action can be a dummy action)
-        if (onChainState.whoseTurn == msg.sender){
-            applyActionToChainState(action);
+  AppState cashOutState; 
+   constructor(bytes32 _merketRoot, uint _dataPacketsCount) payable{
+        merkelRoot = _merketRoot;
+        price = msg.value;
+        merchant = msg.sender;
+        dataPacketsCount = _dataPacketsCount;
+        contractAddress = address(this);
+        
+   }
+   
+   function enter() payable public {
+       require(msg.value == price);
+       client = msg.sender;
+   }
+   
+   function stateToDigest(AppState appState) public returns (bytes32){
+        bytes32 inputAppHash = keccak256(abi.encode(appState));
+        return keccak256(contractAddress, inputAppHash);
+   }    
+   
+   function cashOut(AppState appState, bytes sig) payable public{
+       require(!cashOutDispute );
+    //   !contentDispute);
+       bytes32 digest = stateToDigest(appState);
+       require(recoverSigner(digest, sig) == counterPartyOf(msg.sender));
+       cashOutState = appState;
+       timeout = now + 3600;
+   }
+   
+   function disputeContent(AppState appState, bytes sig){
+       require(msg.sender == client);
+       bytes32 digest = stateToDigest(appState);
+       require(recoverSigner(digest, sig) == merchant);
+       contentDispute = appState.merkelLeaf;
+       
+       appState.clientBalance = price * 2;
+       appState.merchantBalance = 0;
+       cashOutState = appState;
+   }
+   
+//   respondDisputeContent()
+   
+   function disputeCashOut(AppState appState, bytes sig){
+        // bunch of require not settleds everywhere
+        require(cashOutDispute);
+        bytes32 digest = stateToDigest(appState);
+        require(recoverSigner(digest, sig) == counterPartyOf(msg.sender));
+        if(appState.nonce > cashOutState.nonce){
+            // full punishment or naw?
+            cashOutState = appState;
+            settled = true;
         }
-        startDispute();
-    } else if (disputer == opponentOf(gameState, msg.sender)){
-        onChainState.winner = msg.sender;
-    }
- 
-    return true;
-  }
-  
-      modifier updateTimeout (){
-        timeout = now + timeoutInterval;
-        emit UpdateTimeout(timeout, onChainState.whoseTurn);
-        _;
-    }
-    
-    function claimTimeout() payout payable{
-        require(onChainState.winner == address(0));
-        require(onChainState.whoseTurn == opponentOf(onChainState, msg.sender));
-        require(now > timeout);
-        onChainState.winner = msg.sender;
-    }
+   }
+   
+   function claimFunds() payable public {
+       if (!settled){
+           require(cashOutDispute);
+        //   || contentDispute);
+           require((now > timeout));
+       }
 
-    function test(string s) returns(bytes32){
-        return keccak256(s);
-    }
-
-    function testGetClues() public returns (bool[5]){
-        return onChainState.clues;
-    }
-    
-       function stringToUint(string s) constant returns (uint result) {
-        bytes memory b = bytes(s);
-        uint i;
-        result = 0;
-        for (i = 0; i < b.length; i++) {
-            uint c = uint(b[i]);
-            if (c >= 48 && c <= 57) {
-                result = result * 10 + (c - 48);
-            }
-        }
-    }
-
-  modifier payout() {
-      _;
-      if(onChainState.winner!= address(0)) {
-          onChainState.winner.transfer(address(this).balance);
-          onChainState.gameStarted = false;
-      }
-  }
-  
-  
-    function acceptGame() public payable updateTimeout {
-      require(msg.value >= onChainState.stake);
-      require(!onChainState.gameStarted);
-      onChainState.gameStarted = true;
-      onChainState.whoseTurn = msg.sender;
-      onChainState.guesser = msg.sender;
-      emit AcceptGame(getOnChainState());
-  }
-  
-    function applyAction(GameState gameState, Action action)public view turnTaker(gameState, action.sender) returns(GameState){
-      if(action.actionType == ActionType.GUESS){
-          return submitGuess(gameState, action);
-      } else if (action.actionType == ActionType.CLUE){
-          return submitClue(gameState, action);
-      } else if (action.actionType == ActionType.REVEAL){
-          return reveal(gameState, action);
-      }
-
-  }
-  
-  function submitGuess( GameState gameState, Action action) pure public returns  (GameState){
-        require(action.guess < 100);
-        require(action.guess > 0);
-        gameState.guesses[gameState.seq/2] = action.guess;
-        return gameState;
-  }
-    function submitClue( GameState gameState, Action action) pure public returns  (GameState){
-        // todo: divide by 2 7 round down
-        require(gameState.seq < 10);
-        gameState.clues[gameState.seq/2] = action.clue;
-        return gameState;
-  }
-  
-  function reveal(GameState gameState, Action action) returns (GameState){
-    //   why do I have this?
-    //   require(msg.sender == gameState.dealer);
-      if (keccak256(action.secret) != gameState.secretHash ){
-          gameState.winner = opponentOf(gameState, action.sender);
-     
-      } else {
-             //   getnumber
-          uint winningNumber = stringToUint(action.secret) % 100;
-          uint8 currentGuess;
-          bool currentClue;
-          for (uint i=0; i<gameState.guesses.length; i++) {
-              
-            currentGuess = gameState.guesses[i];
-            currentClue = gameState.clues[i];
-            
-            if (currentGuess == winningNumber || currentGuess==0){
-                gameState.winner = gameState.guesser;
-                return gameState;
-            }
-    
-            
-            if ( (currentGuess < winningNumber && currentClue) ||(currentGuess > winningNumber && !currentClue) ) {
-                gameState.winner = gameState.guesser;
-                return gameState;
-            }
-          }
-        // TODO edgecase for if he doesn't submit last clue ? meh
-        // if gueser didn't make all of his guesses yet, guesser wins (check for zeros? requrie this up front)
-        // otherwise, dealer wins,
-      }
-           return gameState;
-  }
-    
-    function opponentOf(GameState gameState, address player)public pure returns(address){
-        if (player == gameState.dealer){
-            return gameState.guesser;
-        } else if (player== gameState.guesser){
-            return gameState.dealer;
+       client.transfer(cashOutState.clientBalance);
+       merchant.transfer(cashOutState.merchantBalance);
+       
+   }
+   
+    function counterPartyOf(address user)public returns(address){
+        if (user == merchant){
+            return client;
+        } else if (user== client){
+            return user;
         }
         require(false);
     }
-    
-
-      
-  
-  modifier turnTaker(GameState gameState, address sender) {
-      require(gameState.winner == address(0));
-      require(sender == gameState.whoseTurn);
-      require(gameState.gameStarted);
-      gameState.seq++;
-      gameState.whoseTurn = opponentOf(gameState, sender);
-      _;    
-  }
-  
-      // Signature methods
-
-    function splitSignature(bytes sig)
-        internal
-        pure
+     function splitSignature(bytes sig)
+       public
         returns (uint8, bytes32, bytes32)
     {
         require(sig.length == 65);
@@ -289,9 +124,8 @@ contract GuessNumber {
         return (v, r, s);
     }
 
-    function recoverSigner(bytes32 message, bytes sig)
-        internal
-        pure
+    function recoverSigner (bytes32 message, bytes sig)
+     public
         returns (address)
     {
         uint8 v;
@@ -302,11 +136,10 @@ contract GuessNumber {
 
         return ecrecover(message, v, r, s);
     }
+    
+    
+  
+    
+  
 
-    // Builds a prefixed hash to mimic the behavior of eth_sign.
-    function prefixed(bytes32 hash) internal pure returns (bytes32) {
-        return keccak256("\x19Ethereum Signed Message:\n32", hash);
-    }
-
- 
 }
