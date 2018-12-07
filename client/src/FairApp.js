7d809952c93bc83d529bf0d831feac3c8b426464
Daniel Goldman <dzgoldman@wesleyan.edu>
2018-12-07 22:57:04 +0800

    first commit! truffle, react, python websockets

---
+++ client/src/FairApp.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import "./App.css";
+import { ethers, utils } from "ethers";
+
+window.utils = utils;
+window.ethers = ethers;
+
+class FairApp extends Component {
+
+
+  render() {
+    return (
+      <div className="App">
+       test app
+
+
+      </div>
+    );
+  }
+}
+
+export default FairApp;
+
